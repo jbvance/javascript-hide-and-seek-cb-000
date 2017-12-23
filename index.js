0d3117bf3d531a7b709e25fca1485b8ea2ebccb9
Jason Vance <jbvance@gmail.com>
2017-12-23 10:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function increaseRankBy(n) {
     .querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    int = lis[i].innerHTML;
+    let int = lis[i].innerHTML;
     lis[i].innerHTML = (parseInt(int, 10) + n).toString();
   }
 }
