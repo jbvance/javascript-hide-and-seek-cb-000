d6e39bcac671adca5c445700e83ccae47920d060
Jason Vance <jbvance@gmail.com>
2017-12-23 10:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target');
 }
+
+funtion increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(list[i].innerHtml) + n).toString()
+  }
+}
