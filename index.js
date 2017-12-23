730885c7913ac045bf1d3a42fc6a8232621672db
Jason Vance <jbvance@gmail.com>
2017-12-23 10:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
 
   while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
+    node = nextNode;
+    nextNode = node.children[0];
   }
 
-  return node
+  return node;
 }
